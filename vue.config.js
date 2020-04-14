49c25625647a474fadbf6e29e5e8da962514253c
Mucka <matus@mucka.sk>
2020-04-14 18:46:25 +0200

    fix class names

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/camelcase */
+module.exports = {
+    chainWebpack: config => {
+        config.optimization.minimizer("terser").tap(args => {
+            const { terserOptions } = args[0];
+            terserOptions.keep_classnames = true;
+            terserOptions.keep_fnames = true;
+            return args;
+        });
+    },
+};
